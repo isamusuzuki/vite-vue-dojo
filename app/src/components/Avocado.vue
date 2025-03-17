e2b58f7ea48a6749650097f3f75939db5e235522
Isamu Suzuki <isamusuzuki@outlook.com>
2025-03-17 12:32:15 +0000

    feat: Avocado, Banana

---
+++ app/src/components/Avocado.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increment = () => {
+    count.value += 1
+}
+const decrement = () => {
+    count.value -= 1
+}
+</script>
+
+<template>
+    <a href="/">
+        <svg viewBox="0 0 24 24" width="24" height="24" style="fill: gray;">
+            <use href="#chevron-left-circle" />
+        </svg>
+    </a>
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-3 is-offset-1">
+                    <nav class="level">
+                        <div class="level-left">
+                            <button
+                                class="level-item button is-light"
+                                @click="decrement"
+                            >
+                                -
+                            </button>
+                            <span class="level-item is-size-3" v-text="count"></span>
+                            <button
+                                class="level-item button is-light"
+                                @click="increment"
+                            >
+                                +
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+
---
+++ app/src/components/Banana.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import InputKeyword from './InputKeyword.vue'
+import SimpleCounter from './SimpleCounter.vue'
+</script>
+
+<template>
+    <a href="/">
+        <svg viewBox="0 0 24 24" width="24" height="24" style="fill: gray;">
+            <use href="#chevron-left-circle" />
+        </svg>
+    </a>
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-3 is-offset-1"><simple-counter></simple-counter></div>
+            </div>
+            <div class="columns">
+                <div class="column is-3 is-offset-1"><simple-counter></simple-counter></div>
+            </div>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-4 is-offset-1">
+                    <input-keyword></input-keyword>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
